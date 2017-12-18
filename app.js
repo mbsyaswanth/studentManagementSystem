a1d0a350289cf9c72630059719e3c32c3a338dd1
mbsyaswanth <mbs.yaswanth@gmail.com>
2017-12-18 09:47:19 +0530

    first commit from commandline on this project

---
+++ app.js
@@ -0,0 +1,23 @@
+var app = angular.module('myApp', ['ngRoute']);
+app.config(function($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl: 'index.html'
+	}).when('/login', {
+		templateUrl: 'login.html'
+	}).when('/employee', {
+		templateUrl: 'resources/employee.html',
+	}).when('/payslips', {
+		templateUrl: 'resources/payslips.html',
+	}).when('/createEmployee', {
+		templateUrl: 'resources/createEmployee.html',
+	}).when('/viewEmployee', {
+		templateUrl: 'resources/viewEmployeeList.html',
+	}).when('/updateEmployee', {
+		templateUrl: 'resources/updateEmployee.html',
+	});
+});
+app.controller("MainController",['$scope',function($scope){
+	
+	
+	
+}]);
